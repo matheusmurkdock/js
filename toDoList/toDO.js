b5b78133686ca45685541b1ac0cfc6ea1883f05c
matheusmurkdock <matheusmurk@protonmail.com>
2025-12-27 12:41:28 +0530

    the user input is usable

--- toDoList/toDO.js
+++ toDoList/toDO.js
@@ -7,9 +7,34 @@ const arrTasks = [
 
 console.log(arrTasks)
 
+const eventAdder = function(youknow) {
+  const taskContainer = document.querySelector(".tasks-region");
+  const div = document.createElement("div");
+  const divTwo = document.createElement("div");
+  let input = document.createElement("input");
+  input.type = "checkbox";
+  let span = document.createElement("span");
+  span.textContent = youknow;
+  span.class = "check-text";
+  const buttonEdit = document.createElement("button");
+  buttonEdit.class = "edit-task-btn";
+  buttonEdit.textContent = "Edit";
+  const buttonDel = document.createElement("button");
+  buttonDel.class = "delete-task-btn";
+  buttonDel.textContent = "Delete";
+  divTwo.append(input)
+  divTwo.append(span)
+  div.append(divTwo)
+  div.append(buttonEdit)
+  div.append(buttonDel)
+  taskContainer.append(div)
+
+}
+
 addBtn.addEventListener("click", function() {
   let taskInput = document.querySelector("#task-input");
   arrTasks.push(taskInput.value);
+  eventAdder(taskInput.value);
   taskInput.value = "";
-  console.log(arrTasks)
+  console.log(arrTasks);
 })
