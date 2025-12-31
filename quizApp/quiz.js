7991c838b4b3f1c31342ed6e983625c9eb2e1472
matheusmurkdock <matheusmurk@protonmail.com>
2025-12-31 10:47:31 +0530

    got the jquery to work

--- quizApp/quiz.js
+++ quizApp/quiz.js
@@ -47,7 +47,7 @@ var opts = document.getElementsByClassName("opt")
 console.log(opts)
 
 // function for next button
-var i = 0;
+var i = 1;
 
 const repeater = function() {
   setTimeout(() => {
@@ -63,7 +63,34 @@ const repeater = function() {
     if ( i < arrQue.length) {
       repeater()
     }
+  },  8000);
+}
+
+$(":button").click(function(){
+  let currObj = arrQue[1];
+  let currAns = currObj.ans
+  console.log(`${currAns} is the Answer`)
+  if (this.textContent == currAns) {
+    console.log("Correct Ans")
+  } else {
+    console.log("Wrong Ans")
   }
-    
-  }, 8000);
+})
+
+next.onclick = function(){
+    let currObj = arrQue[i]
+    if (i < arrQue.length) {
+      i++;
+    } else {
+    return i = 1; // this wil be changed to show the ends Screen
+    }
+    let currQue = currObj["que"]
+    question.textContent = currQue
+    for (t=0; t < opts.length; t++) {
+      opts[0].textContent = currObj.opt[0]
+      opts[1].textContent = currObj.opt[1]
+      opts[2].textContent = currObj.opt[2]
+      opts[3].textContent = currObj.opt[3]
+    }
+
 }
